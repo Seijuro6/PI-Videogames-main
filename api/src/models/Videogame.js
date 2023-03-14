3b06824ff2fea59cc73e519f364a528a412add42
Harold Cadena <astarot6.6@hotmail.com>
2023-03-14 09:38:21 -0500

    modelos

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },    
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    Descripción: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Plataformas: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Imagen: {
+      type: DataTypes.CIDR,
+      allowNull: false,
+    },
+    FechaDeLanzamiento: {
+      type: DataTypes.DATEONLY,
+      defaultValue: DataTypes.DATE,
+      allowNull: false,
+    },
+    Rating: {
+      type: DataTypes.FLOAT,
       allowNull: false,
     },
   });
 };
+
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const {API_KEY} = process.env;
+const axios = require('express')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +9,16 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.get('/get', async (req, res) => {
+
+    try {
+        const allCharacters = await axios(`https://api.rawg.io/api/games?key=27f8cfa1683d4226939e9799677d7b15`);
+          return res.json(allCharacters);
+    } catch (error) {
+          return res.json(error);
+        }
+})
 
 
 module.exports = router;
