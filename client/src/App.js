a10b23213378b3eeea63a3e41b9dd021bc4a857c
Harold Cadena <astarot6.6@hotmail.com>
2023-03-21 12:45:15 -0500

    cambio de script en pakage.json y ordenamiento archivo

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { Route } from "react-router";
+import Home from "./components/Home/Home";
+import Landing from "./components/Landing/Landing";
+
 function App() {
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+     
+      {/* <Route exact path='/'>   
+        <Landing />
+      </Route>  ---- con la siguiente forma o de pueden pasar props */}
+      <Route exact path='/' component={Landing}/>
+     
+      {/* <Route path='/home'>
+        <Home />
+      </Route> ----------con la siguiente forma si se pueden psar props--------- */}
+      <Route path='/home' render={() => <Home/>} />
+
     </div>
   );
 }
